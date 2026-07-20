3cbe11c2733c934265199e526d52456014c0e6dc
kk <richkittyshit@vk.com>
2026-07-20 23:28:30 +0300

    refactor: drop the modmenu update checker absent before modmenu 10

--- src/main/java/ru/kirushkinx/cistiertagger/modmenu/ModMenuImpl.java
+++ src/main/java/ru/kirushkinx/cistiertagger/modmenu/ModMenuImpl.java
@@ -2,22 +2,13 @@ package ru.kirushkinx.cistiertagger.modmenu;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import com.terraformersmc.modmenu.api.UpdateChecker;
-import com.terraformersmc.modmenu.api.UpdateInfo;
 import org.jetbrains.annotations.NotNull;
 import ru.kirushkinx.cistiertagger.gui.screen.ConfigScreen;
 
 public class ModMenuImpl implements ModMenuApi {
 
-    private static final UpdateInfo UPDATE_INFO = new UpdateInfoImpl();
-
     @Override
     public @NotNull ConfigScreenFactory<?> getModConfigScreenFactory() {
         return ConfigScreen::create;
-    }
-
-    @Override
-    public UpdateChecker getUpdateChecker() {
-        return () -> UPDATE_INFO;
     }
 }
