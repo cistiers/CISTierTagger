e1063ec7ff5654e82635e1c96bd7864678521cee
kk <richkittyshit@vk.com>
2026-07-20 23:28:30 +0300

    chore(build): target the branch at 1.20.3 on java 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,13 +11,13 @@ base {
 }
 
 java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
     withSourcesJar()
 }
 
 tasks.withType<JavaCompile>().configureEach {
     options.encoding = "UTF-8"
-    options.release.set(21)
+    options.release.set(17)
 }
 
 repositories {
